fd0ade64ecd9704daa36a9c93768b25f3110090a
livanvaldes084@gmail.com <livanalejandro084>
2019-12-12 14:53:14 -0300

    Commit

--- lib/model/pizza_model.dart
+++ lib/model/pizza_model.dart
@@ -9,9 +9,9 @@ class PizzaModel{
 }
 
 List<PizzaModel> pizzaItems = [
-  PizzaModel(title:"Pizzas 1", picture:"assets/img/pizza.png", price: '5.00', description: 'Pain boulanger, 1 steak haché 130gr frais origine France, oignons'),
-  PizzaModel(title:"Pizzas 2", picture:"assets/img/pizza.png", price: '6.00', description: 'Pain boulanger, 1 steak haché 130gr frais origine France, oignons'),
-  PizzaModel(title:"Pizzas 3", picture:"assets/img/pizza.png", price: '10.00', description: 'Pain boulanger, 1 steak haché 130gr frais origine France, oignons'),
-  PizzaModel(title:"Pizzas 4", picture:"assets/img/pizza.png", price: '12.00', description: 'Pain boulanger, 1 steak haché 130gr frais origine France, oignons'),
-  PizzaModel(title:"Pizzas 5", picture:"assets/img/pizza.png", price: '20.00', description: 'Pain boulanger, 1 steak haché 130gr frais origine France, oignons'),
+  PizzaModel(title:"Pizzas 1", picture:"assets/img/1.png", price: '5.00', description: 'Pain boulanger, 1 steak haché 130gr frais origine France, oignons'),
+  PizzaModel(title:"Pizzas 2", picture:"assets/img/2.png", price: '6.00', description: 'Pain boulanger, 1 steak haché 130gr frais origine France, oignons'),
+  PizzaModel(title:"Pizzas 3", picture:"assets/img/3.png", price: '10.00', description: 'Pain boulanger, 1 steak haché 130gr frais origine France, oignons'),
+  PizzaModel(title:"Pizzas 4", picture:"assets/img/4.png", price: '12.00', description: 'Pain boulanger, 1 steak haché 130gr frais origine France, oignons'),
+  PizzaModel(title:"Pizzas 5", picture:"assets/img/5.png", price: '20.00', description: 'Pain boulanger, 1 steak haché 130gr frais origine France, oignons'),
 ];
